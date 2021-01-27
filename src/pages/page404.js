899d11f1727ef30202a65f9cde1dd89b82def2c5
Wurikos <50768680+Wurikos@users.noreply.github.com>
2021-01-27 18:06:20 +0300

    Changes

---
+++ src/pages/page404.js
@@ -0,0 +1,55 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { RawHtml } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"404"} />
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+		<RawHtml>
+			<style place={"endOfHead"} rawKey={"601181203b82557eb2b6f047"}>
+				{"body {\nbackground-color:red}"}
+			</style>
+			<meta
+				charset={""}
+				name={"keywords"}
+				content={"новы слова"}
+				place={"endOfHead"}
+				rawKey={"60118146bc933ddf757cb6a4"}
+			/>
+			<script place={"endOfBody"} rawKey={"6011815720043ccd7d24330d"}>
+				{"alert (\"test\")"}
+			</script>
+		</RawHtml>
+	</Theme>;
+});
---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60117d9c10485c001e656829",
+				"60117d9c10485c001e65682b"
+			]
+		},
+		"60117d9c10485c001e656829": {
+			"id": "60117d9c10485c001e656829",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60117d9c10485c001e65682b": {
+			"id": "60117d9c10485c001e65682b",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
